1ef4a38d6e2b5ed3f0269ccf1d5e31f03917528f
SkyGamingCell <42546247+SkyGamingCell@users.noreply.github.com>
2018-08-20 21:54:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const botconfig = require("./botconfig.json");
 const Discord = require("Discord.js");
-const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 let coins = require("./coins.json");
