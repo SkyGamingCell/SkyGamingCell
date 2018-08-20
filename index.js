36294d6963c84087140e0064f11f8d416857e5e0
SkyGamingCell <42546247+SkyGamingCell@users.noreply.github.com>
2018-08-20 19:05:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,215 @@
+
+const botconfig = require("./botconfig.json");
+const Discord = require("Discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
+
+
+ bot.on('ready', async() => {
+   console.log(`${bot.user.username} is online!`);
+  bot.user.setGame(`${bot.guilds.size} servers | SkyGamingCell Videos `);
+});
+
+// Updates the bot's status if he joins a server
+bot.on("guildCreate", guild => {
+   bot.user.setGame(`${bot.guilds.size} servers | SkyGamingCell Videos `);
+});
+
+/// Updates the bot's status if he leaves a servers
+bot.on("guildDelete", guild => {
+    bot.user.setGame(`${bot.guilds.size} servers | SkyGamingCell Videos`);
+});
+
+bot.on("message",async message =>{
+const logsCommands = bot.channels.get(botconfig.logsChannelID);
+const logsCommands2 = bot.channels.get(botconfig.logsChannelID);
+
+client.login(process.env.BOT_TOKEN);
+
+  if(message.channel.type == "dm") {
+    console.log(`${message.author.tag} שלח לי הודעה פרטית!`);
+    return logsCommands.send(`${message.author.tag} שלח לי הודעה פרטית!`);
+}
+
+ let prefix = botconfig.prefix;
+ let messageArray = message.content.split(" ");
+ let cmd = messageArray[0];
+ let args = messageArray.slice(1);
+
+ if(cmd === `${prefix}kick`){
+   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+   if(!kUser) return message.channel.send("Can't find user!");
+   let kReason = args.join(" ").slice(22);
+   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!")
+   if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+let kickEmbed = new Discord.RichEmbed()
+.setDescription("~kick~")
+.setColor("#42f4e8")
+.addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Kicked In", message.channel)
+.addField("Time", message.createdAt)
+.addField("Reason", kReason);
+
+message.delete().catch(O_o=>{});
+
+
+
+let kickChannel = message.guild.channels.find(`name`, "incidents")
+if(!kickChannel) return message.channel.send("Can't find incidents channel")
+
+message.guild.member(kUser).kick(kReason);
+kickChannel.send(kickEmbed)
+
+console.log(`${message.author.tag} Ask for help from the bot`);
+    return logsCommands.send(`${message.author.tag} Ask help from the bot`);
+
+   return;
+ }
+
+if(!coins[message.author.id])
+coins[message.author.id] = {
+  coins: 0
+};
+
+
+let coinAmt = Math.floor(Math.random() * 15) +1;
+let baseAmt = Math.floor(Math.random() * 15) +1;
+console.log(`${coinAmt} ; ${baseAmt}`);
+
+if(coinAmt ===baseAmt){
+coins[message.author.id] = {
+  coins: coins[message.author.id].coins + coinAmt
+};
+fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+  if (err) console.log(err)
+});
+let coinEmbed = new Discord.RichEmbed()
+.setAuthor(message.author.username)
+.setColor("#42f4e8")
+.addField("💰", `${coinAmt} coin added!`);
+
+message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+}
+
+if(cmd === `${prefix}report`){
+
+
+
+let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!rUser) return message.channel.send("Couldn't find user.");
+let reason = args.join(" ").slice(22);
+
+let reportEmbed =  new Discord.RichEmbed()
+.setDescription("Reports")
+.setColor("#4286f4")
+.addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+.addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+.addField("Channel", message.channel)
+.addField("Time", message.createdAt)
+.addField("Reason", reason);
+
+
+let reportschannel = message.guild.channels.find(`name`,"reports");
+if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+
+message.delete().catch(O_o=>{});
+reportschannel.send(reportEmbed)
+
+  return;
+}
+
+if(cmd === `${prefix}ban`){
+
+let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!bUser) return message.channel.send("Can't find user!");
+let bReason = args.join(" ").slice(22);
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!")
+if(bUser.hasPermission("MANAGE_MEMBERS")) return message.channel.send("That person can't be kicked!");
+
+let banEmbed = new Discord.RichEmbed()
+.setDescription("~Ban~")
+.setColor("#5443ef")
+.addField("Banned User", `${bUser} with ID ${bUser.id}`)
+.addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Banned In", message.channel)
+.addField("Time", message.createdAT)
+.addField("Reason", bReason);
+
+let incidentChannel = message.guild.channels.find(`name`, "incidents")
+if(!incidentChannel) return message.channel.send("Can't find incidents channel")
+
+message.delete().catch(O_o=>{});
+
+
+message.guild.member(bUser).ban(bReason);
+incidentchannel.send(banEmbed);
+}
+
+if(cmd === `${prefix}serverinfo`){
+     let sicon = message.guild.iconURL;
+     let serverembed = new Discord.RichEmbed()
+     .setDescription("Server Information")
+     .setColor("#75aaff")
+     .setThumbnail(sicon)
+     .addField("Server Name", message.guild.name)
+     .addField("createdAt", message.guild.createdAt)
+     .addField("Join Date", message.member.joinedAt)
+     .addField("Member Count", message.guild.memberCount);
+
+
+
+     return message.channel.send(serverembed);
+   }
+
+   if(message.content == 'לך תזדיין'){
+
+     message.delete().catch(O_o=>{});
+     console.log(`${message.author.tag} השתמש במילה לך תזדיין, מגיע אזהרה או העפה, תלוי במקרה.`);
+     return logsCommands.send(`${message.author.tag}  השתמש במילה לך תזדיין, מגיע אזהרה או העפה, תלוי במקרה.`);
+   }
+
+if(cmd === `${prefix}botinfo`){
+
+let bicon = bot.user.displayAvatarURL;
+let botembed = new Discord.RichEmbed()
+.setDescription("Bot Information")
+.setColor("#4286f4")
+.setThumbnail(bicon)
+.addField("Bot Name", bot.user.username)
+.addField("Created On", bot.user.createdAt)
+.addField("Created By", "<@461151799924228108>");
+
+
+return message.channel.send(botembed);
+ }
+
+ if(cmd === `${prefix}stw_helper`){
+
+ let botembed = new Discord.RichEmbed()
+ .setDescription("עזרה בסייב דה וורלד")
+ .setColor("#4286f4")
+ .addField("רמות גבוהות", "<@461151799924228108>")
+ .addField("רמות נמוכות", "<@354982773951430656>")
+
+ if(message.content == 'שון'){
+
+   message.channel.sendMessage('מה? ' + message.author);
+ }
+
+ if(message.content == 'יואב'){
+
+   message.channel.sendMessage('<@270928517896732672> ' + message.author + ', אה כפרה?');
+ }
+
+ return message.channel.send(botembed);
+ }
+
+
+});
+
+bot.login(botconfig.token);
